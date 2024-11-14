2fe67e861101e49d647c4badb605eac94e8f0434
Inder-designer <bhupinder7814@gmail.com>
2024-11-14 15:53:44 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Layout from "./Layout/Layout.jsx";
+import Home from "./Pages/Home/Home.jsx";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+const flowbite = require("flowbite-react/tailwind");
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx}",
+    "./index.html",
+    "./node_modules/flowbite/**/*.{js,jsx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx}",
+    flowbite.content(),
+  ],
+  theme: {
+    screens: {
+      '2xs': "400px",
+      xs: "470px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1140px",
+      '2xl': "1280px",
+      '3xl': "1320px",
+      '4xl': "1400px",
+      '5xl': "1480px",
+    },
+    extend: {
+      colors: {
+        'primary': '#7065F0',
+        'secondary': '#100A55',
+        'text1': '#000929',
+        'text2': '#777777',
+        'border1': '#E0DEF7',
+      },
+      fontFamily: {
+        'poppins': ["Poppins", 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'jakarta': ['Plus Jakarta Sans', 'sans-serif'],
+      },
+      textShadow: {
+        'text-custom': '-.03ex 0 currentColor, .03ex 0 currentColor',
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.text-shadow-custom': {
+          textShadow: '-.03ex 0 currentColor, .03ex 0 currentColor',
+        },
+      });
+    },
+    require('flowbite/plugin'),
+    flowbite.plugin(),
+  ],
+};
