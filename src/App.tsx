64cb7f0d13fbcbad3ef616827700887f3000670c
Inder-designer <bhupinder7814@gmail.com>
2024-11-19 17:34:34 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import Pricing from "./Pages/Pricing/Pricing.tsx";
 import AboutUs from "./Pages/AboutUs/AboutUs.tsx";
 import ContactUs from "./Pages/ContactUs/ContactUs.tsx";
 import Gallery from "./Pages/Gallery/Gallery.tsx";
+import EstimateRevenue from "./Pages/EstimateRevenue/EstimateRevenue.tsx";
+import CleaningMaintenance from "./Pages/Services/CleaningMaintenance.tsx";
+import InteriorDesign from "./Pages/Services/InteriorDesign.tsx";
+import ManagementSupport from "./Pages/Services/ManagementSupport.tsx";
+import ListingManagement from "./Pages/Services/ListingManagement.tsx";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -34,6 +39,26 @@ const App = () => {
         {
           path: "/gallery",
           element: <Gallery />,
+        },
+        {
+          path: "/estimate-revenue",
+          element: <EstimateRevenue />,
+        },
+        {
+          path: "/services/cleaning-maintenance",
+          element: <CleaningMaintenance />,
+        },
+        {
+          path: "/services/interior-design",
+          element: <InteriorDesign />,
+        },
+        {
+          path: "/services/management-support",
+          element: <ManagementSupport />,
+        },
+        {
+          path: "/services/listing-management",
+          element: <ListingManagement />,
         },
       ],
     },
